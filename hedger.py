cf50043f314558f7ef8eeeb5b40dd9abdbcb71ea
konstantin <ketterko@gmail.com>
2021-10-05 17:57:32 +0200

    start

---
+++ hedger.py
@@ -0,0 +1,88 @@
+import math
+import time
+import csv
+import json
+
+class Hedger:
+    balance = 18932
+    coverage_range = 0.1
+    #timestamp,value
+    total_balance_cache = [0,0]
+
+    def save_stats(self,price,total_balance,margin_used):
+        with open('stats.csv','a') as fd:
+            fd.write("{},{},{},{:.2f},{} \n".format(time.time(),price,total_balance,(total_balance/(1*10**8))*price,margin_used))
+
+    def __init__(self):
+        with open('config.json') as json_file:
+            data = json.load(json_file)
+            self.balance = data["balance"]
+            self.coverage_range = data["coverage_range"]
+            self.exchange = LNM(data["LNMToken"])
+
+    def get_total_balance(self):
+        #save total balance for 10 seconds
+        if time.time() - self.total_balance_cache[0] < 10:
+            return self.total_balance_cache[1]
+        else:
+            total_balance = self.balance + self.exchange.get_available_margin() + self.exchange.get_running_position_total_margin()
+            self.total_balance_cache = [time.time(),total_balance]
+            return total_balance
+
+    def get_current_balance(self):
+        return self.balance + self.exchange.get_available_margin() + self.exchange.get_running_position_total_margin()
+
+    def get_balance_usd_value(self):
+        total_balance = self.get_total_balance()
+        current_bid_price = self.exchange.get_current_price()
+        return (total_balance/(1*10**8)) * current_bid_price
+
+    def update(self):
+
+        total_balance = self.get_total_balance()
+        price = self.exchange.get_current_price()
+        margin_used = self.exchange.get_margin_used()
+
+        self.save_stats(price,total_balance,margin_used)
+
+        print("running with {:.2f}% coverage".format(self.exchange.get_position_coverage(self.get_current_balance())*100))
+        print("current total balance of {} sats with value of: {:.2f} USD with Bitcoin price @{} USD".format(total_balance,self.get_balance_usd_value(),self.exchange.get_current_price()))
+        if self.exchange.are_positions_running() == True:
+            if self.exchange.is_position_coverage_within_range(self.coverage_range,self.get_current_balance()) == False:
+                print("short running but not within the coverage range!")
+                self.exchange.close_position()
+                self.exchange.open_short(int(self.get_current_balance()/self.exchange.default_leverage))
+        else:
+            print("no shorts running!")
+            short_data = self.exchange.open_short(int(self.get_current_balance()/self.exchange.default_leverage))
+
+    """
+    def add(self,x):
+        self.balance += x
+        self.fund_lnm()
+
+    def remove(self,x):
+        self.balance -= x
+        #self.withdraw_lnm()
+
+    def _get_min_total_required_balance(self):
+        return math.ceil(self.balance*0.02)
+
+    def fund_lnm(self):
+        missing_funds = self._get_min_total_required_balance() - self.margin_used
+        if missing_funds > 0:
+            self.margin_available = max(1000,missing_funds) + self.margin_available
+            print("lnm missing",max(1000,missing_funds),"sats")
+        print("balance:",self.balance," lnm available:",self.margin_available, " lnm margin used:",self.margin_used)
+
+    def withdraw_lnm(self):
+        unnecessary_funds = self.lnm_balance - self._get_min_total_required_balance()
+        if unnecessary_funds >= 1000:
+            print("withdrawing",unnecessary_funds," sats from lnmarkets")
+            self.lnm_balance -= unnecessary_funds
+        print("balance:",self.balance," lnm:",self.lnm_balance)
+    """
+
+if __name__ == "__main__":
+    h = Hedger()
+    h.update()
---
+++ lnm.py
@@ -0,0 +1,118 @@
+import requests
+import json
+
+class LNM:
+    default_leverage = 50
+    unit_margin = 41.8
+
+    def __init__(self,LNMToken):
+        self.LNMToken = LNMToken
+        self.headers = {
+            "Accept": "application/json",
+            "Authorization": "Bearer "+self.LNMToken+""
+        }
+
+    def delete_running_pid(self):
+        self.update_pid("")
+
+    def update_pid(self,pid):
+        with open('lnm.json', 'w') as outfile:
+            json.dump({"running_pid":pid}, outfile)
+
+    def get_running_pid(self):
+        with open('lnm.json') as json_file:
+            data = json.load(json_file)
+            return data["running_pid"]
+
+    def get_current_pl(self):
+        running_positions = self.get_running_positions()
+        if len(running_positions) == 0:
+            return 0
+        else:
+            return running_positions[0]["pl"]
+
+    def get_running_position_total_margin(self):
+        running_positions = self.get_running_positions()
+        margin = 0
+        for position in running_positions:
+            margin += position["margin"] + position["pl"]
+        return margin
+
+    def open_short(self,margin):
+        url = "https://api.lnmarkets.com/v1/futures"
+        payload = {
+            "type": "m",
+            "side": "s",
+            "margin": margin,
+            "leverage": 50
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer "+self.LNMToken
+        }
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+
+        data = response.json()
+        print(data)
+        self.update_pid(data["position"]["pid"])
+        print("opened short position @{} USD with {} sats margin".format(data["position"]["price"],data["position"]["margin"]))
+        return data
+
+    def close_position(self,pid=""):
+        url = "https://api.lnmarkets.com/v1/futures"
+        if pid == "":
+            pid = self.get_running_pid()
+
+        querystring = {"pid":pid}
+        response = requests.request("DELETE", url, headers=self.headers, params=querystring)
+        self.delete_running_pid()
+        data = response.json()
+        print(data)
+        print("closed position with a p&l of {} sats".format(data["pl"]))
+        return response.json()
+
+    def fund(self,amount):
+        pass
+    def withdraw(self,invoice):
+        pass
+
+    def get_running_positions(self):
+        url = "https://api.lnmarkets.com/v1/futures"
+        querystring = {"type":"running"}
+        response = requests.request("GET", url, headers=self.headers, params=querystring)
+        return response.json()
+
+    def are_positions_running(self):
+        if len(self.get_running_positions()) == 0:
+            return False
+        else:
+            return True
+
+    def get_available_margin(self):
+        url = "https://api.lnmarkets.com/v1/user"
+        response = requests.request("GET", url, headers=self.headers)
+        return response.json()["balance"]
+
+    def get_margin_used(self):
+        running_positions = self.get_running_positions()
+        margin = 0
+        for position in running_positions:
+            margin += position["margin"]
+        return margin
+
+    def get_position_coverage(self,balance):
+        return (self.default_leverage* self.get_margin_used())/balance
+
+    def is_position_coverage_within_range(self,range,balance):
+        return abs(self.get_position_coverage(balance) - 1.0) <= range
+
+    def get_optimal_margin(self,balance):
+        return int((int(balance/self.unit_margin) * self.unit_margin))
+    def get_optimal_coverage(self,balance):
+        return self.get_optimal_margin(balance)/balance
+    def get_current_price(self):
+        url = "https://api.lnmarkets.com/v1/futures/history/bid-offer"
+        response = requests.request("GET", url, headers=self.headers)
+        return response.json()[0]["bid"]
